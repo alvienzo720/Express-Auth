f95329663366231474a8dde8d84892787e7eaea0
alvienzo720 <hudson1Dellmac>
2023-01-27 17:54:31 +0300

    Project Starter Code

---
+++ src/controllers/user/currentUserController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { collections } from "../../utils/databaseservices";
+
+
+export async function returnUser(req: Request, res: Response) {
+    console.log("87996")
+
+
+}
---
+++ src/routers/MasterRouter.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import registerUserRouter from './users/registerUserRouter';
+import loginUserRouter from './users/loginUserRouter';
+import currentUserRouter from './users/currentUserRouter';
+
+class MasterRouter {
+  private _router = Router();
+  private _loginUserRouter = loginUserRouter;
+  private _registerUserRouter = registerUserRouter;
+  private _currentUserRouter = currentUserRouter;
+
+  get router() {
+    return this._router;
+  }
+
+  constructor() {
+    this._configure();
+  }
+
+  /**
+  * Connect routes to their matching routers.
+  */
+  private _configure() {
+
+    this._router.use('/register', this._registerUserRouter)
+    this._router.use('/login', this._loginUserRouter);
+    this._router.use('/current-user', this._currentUserRouter)
+  }
+}
+
+export = new MasterRouter().router;
